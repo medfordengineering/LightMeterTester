80a278ac9464a2cda4c8734866f9a2124edfabb4
medfordengineering <schristy@medford.k12.ma.us>
2025-11-05 10:48:09 -0500

    working with functions

--- LDR_Tester/LDR_Tester.ino
+++ LDR_Tester/LDR_Tester.ino
@@ -27,8 +27,11 @@
 #define EW 30
 #define EH 10
 
-#define RAW_TOTAL 50
-#define METER 5
+#define RAW_COUNT 50
+#define METER_PIN 5
+#define CVALUE 250
+#define CAPTURE_DELAY 10
+#define DEFAULT_ASA 400
 
 // These constants, define values needed for the LDR readings and ADC
 #define MAX_ADC_READING 1023
@@ -37,8 +40,25 @@
 //#define LUX_CALC_SCALAR           12518931
 //#define LUX_CALC_EXPONENT         -1.405
 
+int asa = DEFAULT_ASA;
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
+double luxToEv(double lux) {
+  if (lux <= 0) {
+    return -INFINITY;  // Throw exception for negative numbers
+  }
+  return log(lux * asa / CVALUE) / log(2.0);
+}
+
+float rawToLdr(double ldrRaw) {
+  float ldrResistance;
+  float resistorVoltage = (float)ldrRaw / MAX_ADC_READING * ADC_REF_VOLTAGE;
+
+  float ldrVoltage = ADC_REF_VOLTAGE - resistorVoltage;
+
+  return ldrResistance = ldrVoltage / resistorVoltage * REF_RESISTANCE;
+}
 
 void setup() {
   Serial.begin(115200);
@@ -54,33 +74,27 @@ void setup() {
 
 void loop() {
 
-  float resistorVoltage, ldrVoltage;
-  float ldrResistance;
+  //float resistorVoltage, ldrVoltage;
+ // float ldrResistance;
   float ldrLux;
   uint32_t ldrRaw = 0;
 
-  for (int x = 0; x < RAW_TOTAL; x++) {
-    delay(10);
-    ldrRaw += analogRead(METER);
+  for (int x = 0; x < RAW_COUNT; x++) {
+    delay(CAPTURE_DELAY);
+    ldrRaw += analogRead(METER_PIN);
   }
+  ldrRaw /= RAW_COUNT;
 
-  ldrRaw /= RAW_TOTAL;
+//float rawToLdr(doulbe ldrRaw) {
 
-  // RESISTOR VOLTAGE_CONVERSION
-  // Convert the raw digital data back to the voltage that was measured on the analog pin
-  resistorVoltage = (float)ldrRaw / MAX_ADC_READING * ADC_REF_VOLTAGE;
-
-  // voltage across the LDR is the 5V supply minus the 5k resistor voltage
-  ldrVoltage = ADC_REF_VOLTAGE - resistorVoltage;
-
-  // LDR_RESISTANCE_CONVERSION
-  // resistance that the LDR would have for that voltage
-  ldrResistance = ldrVoltage / resistorVoltage * REF_RESISTANCE;
+  float ldrResistance = rawToLdr(ldrRaw);
 
   //float volt = a * max_v / 1023;
   //float lux = (volt * (-4000)) + 18000;
 
   ldrLux = 3.36899 * pow(10, 9) * (pow(ldrResistance, -1.56617));
+
+  double ev = luxToEv(ldrLux);
 
   display.fillRect(BX, BY, BW, BH, SSD1306_BLACK);
   display.setCursor(BX, BY);
@@ -99,17 +113,17 @@ void loop() {
 
   //display.setCursor(VX, VY+ 12);
   //display.println(a);
-  /*
+  /* 
   display.fillRect(SX + 1, SY + 1, SW - 2, SH - 2, SSD1306_BLACK);
   display.setCursor(SX + 3, SY + 2);
   //display.println(speeds[count]);
-  display.println(ev_v);
+  display.println(ev);
 
   display.setTextSize(1);
-
+*/
   display.fillRect(EX, EY, EW, EH, SSD1306_BLACK);
   display.setCursor(EX, EY);
-  //display.println(ev);
+  display.println(ev);
 
   // Serial.print("EV: ");
   //Serial.print(ev);
@@ -117,13 +131,13 @@ void loop() {
   //Serial.print(" AD: ");
   //Serial.print(ad);
 
-  Serial.print(" LuxV: ");
+  //Serial.print(" LuxV: ");
   //Serial.print(luxVM);
 
 
-  Serial.print(" LuxB: ");
-  Serial.println(lux);
+  //Serial.print(" LuxB: ");
+  //Serial.println(lux);
 
-*/
+
   display.display();
 }
